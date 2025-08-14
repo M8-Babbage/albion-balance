0511c411ed067d71359d92c94f5ff14fa4763403
Edwin J. Páez <edwinjpr11@gmail.com>
2025-08-14 17:25:54 -0500

    feat: cambiar forma de arranque

--- index.js
+++ index.js
@@ -1,51 +1,9 @@
-require("dotenv").config();
-const {
-  loadFunctions,
-  loadEvents,
-  loadCommands,
-} = require("./src/utils/loadHandlers.js");
-const connectToMongo = require("./mongo.js");
-const { Client, GatewayIntentBits, Collection } = require("discord.js");
 const express = require("express");
-
 const app = express();
-
-app.get("/", (req, res) => {
-  res.send("Bot en ejecución 🚀");
-});
-
 const PORT = process.env.PORT || 3000;
 
-const client = new Client({
-  intents: [
-    GatewayIntentBits.Guilds,
-    GatewayIntentBits.GuildMessages,
-    GatewayIntentBits.MessageContent,
-  ],
-});
+app.get("/", (req, res) => res.send("Bot online 🚀"));
 
-client.commands = new Collection();
+app.listen(PORT, () => console.log(`Servidor web en puerto ${PORT}`));
 
-(async () => {
-  loadFunctions(client);
-  loadEvents(client);
-  loadCommands(client);
-
-  try {
-    await client.login(process.env.BOT_TOKEN);
-    console.log("Bot iniciado correctamente ✅");
-
-    // Solo iniciar el servidor web después de que el bot esté listo
-    app.listen(PORT, () => {
-      console.log(`Servidor web escuchando en puerto ${PORT}`);
-    });
-  } catch (error) {
-    console.error("Error al iniciar sesión del bot", error);
-  }
-
-  try {
-    await connectToMongo(process.env.MONGODB_URL);
-  } catch (error) {
-    console.error("Error al conectar a la base de datos.", error);
-  }
-})();
+require("./bot.js");
