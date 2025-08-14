40261ff3d770d5e087356eeefdf42909493a023c
Edwin J. Páez <edwinjpr11@gmail.com>
2025-08-14 17:35:02 -0500

    feat: resetear valores iniciales

--- index.js
+++ index.js
@@ -1,9 +1,37 @@
-const express = require("express");
-const app = express();
-const PORT = process.env.PORT || 3000;
+require("dotenv").config();
+const {
+  loadFunctions,
+  loadEvents,
+  loadCommands,
+} = require("./src/utils/loadHandlers.js");
+const connectToMongo = require("./mongo.js");
 
-app.get("/", (req, res) => res.send("Bot online 🚀"));
+const { Client, GatewayIntentBits, Collection } = require("discord.js");
 
-app.listen(PORT, () => console.log(`Servidor web en puerto ${PORT}`));
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent,
+  ],
+});
 
-require("./bot.js");
+client.commands = new Collection();
+
+(async () => {
+  loadFunctions(client);
+  loadEvents(client);
+  loadCommands(client);
+
+  try {
+    await connectToMongo(process.env.MONGODB_URL);
+  } catch (error) {
+    console.error("Error al conectar a la base de datos.", error);
+  }
+
+  try {
+    await client.login(process.env.BOT_TOKEN);
+  } catch (error) {
+    console.error("Error al iniciar sesion del bot", error);
+  }
+})();
